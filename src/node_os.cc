f0047cb1c9341edadc88891fe31f4f00837960bd
Michaël Zasso <targos@protonmail.com>
2018-10-13 11:08:08 -0700

    gotta start with something

--- src/node_os.cc
+++ src/node_os.cc
@@ -19,6 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#include "napi.h"
 #include "node_internals.h"
 #include "string_bytes.h"
 
