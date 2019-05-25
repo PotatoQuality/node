2870a68464c01c89a316d952f5b1348e850b9cba
Michaël Zasso <targos@protonmail.com>
2019-05-25 08:25:59 +0200

    debug test-worker-prof

--- test/parallel/test-worker-prof.js
+++ test/parallel/test-worker-prof.js
@@ -11,18 +11,23 @@ const { spawnSync } = require('child_process');
 // Refs: https://github.com/nodejs/node/issues/24016
 
 if (process.argv[2] === 'child') {
+  console.log('child | in child')
   let files = fs.readdirSync(tmpdir.path);
   const plog = files.filter((name) => /\.log$/.test(name))[0];
   if (plog === undefined) {
     console.error('`--prof` did not produce a profile log for parent thread!');
     process.exit(1);
   }
+  console.log('child | found plog')
   const pingpong = `
   let counter = 0;
   const { Worker, parentPort  } = require('worker_threads');
+  console.log('worker | in worker')
   parentPort.on('message', (m) => {
-    if (counter++ === 1024)
+    if (counter++ === 1024) {
+      console.log('worker | end')
       process.exit(0);
+     }
      parentPort.postMessage(
        m.toString().split('').reverse().toString().replace(/,/g, ''));
   });
@@ -31,11 +36,13 @@ if (process.argv[2] === 'child') {
   const { Worker } = require('worker_threads');
   const data = 'x'.repeat(1024);
   const w = new Worker(pingpong, { eval: true });
+  console.log('child | created worker')
   w.on('message', (m) => {
     w.postMessage(m.toString().split('').reverse().toString().replace(/,/g, ''));
   });
 
   w.on('exit', common.mustCall(() => {
+    console.log('child | worker exited')
     files = fs.readdirSync(tmpdir.path);
     const wlog = files.filter((name) => /\.log$/.test(name) && name !== plog)[0];
     if (wlog === undefined) {
@@ -43,17 +50,21 @@ if (process.argv[2] === 'child') {
                     ' for worker thread!');
       process.exit(1);
     }
+    console.log('child | done')
     process.exit(0);
   }));
   w.postMessage(data);
+  console.log('child | posted first message')
 } else {
+  console.log('parent | in parent')
   tmpdir.refresh();
   const spawnResult = spawnSync(
     process.execPath, ['--prof', __filename, 'child'],
-    { cwd: tmpdir.path, encoding: 'utf8' });
-  assert.strictEqual(spawnResult.stderr.toString(), '',
-                     `child exited with an error: \
-                     ${util.inspect(spawnResult)}`);
+    { cwd: tmpdir.path, encoding: 'utf8', stdio: 'inherit' });
+  console.log('parent | child terminated')
+  //assert.strictEqual(spawnResult.stderr.toString(), '',
+  //                   `child exited with an error: \
+  //                   ${util.inspect(spawnResult)}`);
   assert.strictEqual(spawnResult.signal, null,
                      `child exited with signal: ${util.inspect(spawnResult)}`);
   assert.strictEqual(spawnResult.status, 0,
@@ -62,7 +73,7 @@ if (process.argv[2] === 'child') {
   const files = fs.readdirSync(tmpdir.path);
   const logfiles = files.filter((name) => /\.log$/.test(name));
   assert.strictEqual(logfiles.length, 2);  // Parent thread + child thread.
-
+  console.log('parent | verified logfiles are there')
   for (const logfile of logfiles) {
     const lines = fs.readFileSync(
       join(tmpdir.path, logfile), 'utf8').split('\n');
@@ -75,4 +86,5 @@ if (process.argv[2] === 'child') {
     // for both threads, so 15 seems like a very safe threshold.
     assert(ticks >= 15, `${ticks} >= 15`);
   }
+  console.log('parent | finish')
 }
